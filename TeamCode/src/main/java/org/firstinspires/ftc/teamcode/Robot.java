50c853f7a92af60203dff3e392b823a594dc6d14
hudson-dev <hudson.n.kim@gmail.com>
2022-09-24 21:35:12 -0700

    organized code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Robot {
+    public void init() {
+
+    }
+    public void update() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/drive/roadrunner/trajectorysequence/EmptySequenceException.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.modules.drive.roadrunner.trajectorysequence;
+
+
+public class EmptySequenceException extends RuntimeException { }
