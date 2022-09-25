eff6756ffbcfe52258e36434abd94b79325efdd4
hkim <hudson.n.kim@gmail.com>
2022-09-24 23:49:15 -0700

    working on Drive class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/drive/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/drive/Drive.java
@@ -1,10 +1,50 @@
 package org.firstinspires.ftc.teamcode.modules.drive;
 
+import static org.firstinspires.ftc.teamcode.modules.drive.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.teamcode.modules.drive.DriveConstants.MAX_ANG_VEL;
+import static org.firstinspires.ftc.teamcode.modules.drive.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.teamcode.modules.drive.DriveConstants.TRACK_WIDTH;
+import static org.firstinspires.ftc.teamcode.modules.drive.SampleMecanumDrive.getAccelerationConstraint;
+import static org.firstinspires.ftc.teamcode.modules.drive.SampleMecanumDrive.getVelocityConstraint;
+
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.followers.TrajectoryFollower;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
 import org.firstinspires.ftc.teamcode.modules.Module;
+import org.firstinspires.ftc.teamcode.modules.drive.roadrunner.trajectorysequence.TrajectorySequenceRunner;
+
+import java.util.List;
 
 public class Drive implements Module {
-    @Override
-    public void init() {
+
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+
+    public static double LATERAL_MULTIPLIER = 1;
+
+    public static double VX_WEIGHT = 1;
+    public static double VY_WEIGHT = 1;
+    public static double OMEGA_WEIGHT = 1;
+
+    private TrajectorySequenceRunner trajectorySequenceRunner;
+
+    private static final TrajectoryVelocityConstraint VEL_CONSTRAINT = getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH);
+    private static final TrajectoryAccelerationConstraint ACCEL_CONSTRAINT = getAccelerationConstraint(MAX_ACCEL);
+
+    private TrajectoryFollower follower;
+
+    private DcMotorEx leftFront, leftRear, rightRear, rightFront;
+    private List<DcMotorEx> motors;
+
+    private BNO055IMU imu;
+    private VoltageSensor batteryVoltageSensor;
+
+    public Drive(HardwareMap hardwareMap) {
 
     }
 
