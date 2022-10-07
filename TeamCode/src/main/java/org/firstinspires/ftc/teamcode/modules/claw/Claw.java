b45b5894ce6fb5677a1357e753b39bb5dc95d909
hkim <hudson.n.kim@gmail.com>
2022-10-07 15:35:41 -0700

    added in servos for v4bar and claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/claw/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/claw/Claw.java
@@ -1,4 +1,72 @@
 package org.firstinspires.ftc.teamcode.modules.claw;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.util.MyServo;
+
 public class Claw {
+    MyServo claw;
+
+    public double currentClawAngle = 0.0;
+    public double targetClawAngle = 0.0;
+    public double clawPower = 0.0;
+
+    double openAngle = 50.0;
+    double intakeAngle = 35.0;
+    double closeAngle = 25.0;
+
+    public enum STATE {OPEN, INTAKE, CLOSED}
+    public STATE currentState = STATE.OPEN;
+
+
+    public Claw(HardwareMap hardwareMap) {
+        claw = new MyServo(hardwareMap.servo.get("claw"),"Speed",1,0,1);
+    }
+
+    public void update() {
+        updateClawValues();
+
+        switch (currentState) {
+            case OPEN:
+                setTargetClawAngle(openAngle);
+                break;
+            case INTAKE:
+                setTargetClawAngle(intakeAngle);
+                break;
+            case CLOSED:
+                setTargetClawAngle(closeAngle);
+                break;
+        }
+
+        claw.setAngle(targetClawAngle, clawPower);
+    }
+
+    private void setTargetClawAngle(double angle) {
+        targetClawAngle = angle;
+    }
+
+    private void setTargetClawAngle(double angle, double power) {
+        targetClawAngle = angle;
+        clawPower = power;
+    }
+
+    public double getCurrentClawAngle () {
+        return currentClawAngle;
+    }
+
+    public void updateClawValues() {
+        currentClawAngle = claw.getAngle();
+    }
+
+    public void open() {
+        currentState = STATE.OPEN;
+    }
+
+    public void close() {
+        currentState = STATE.CLOSED;
+    }
+
+    public void intake() {
+        currentState = STATE.INTAKE;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MyServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MyServo.java
@@ -5,10 +5,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class MyServo {
     Servo servo;
     public double speed;
-    public MyServo(Servo servo, String servoType, double loadMultiplier, double ticksPerRadians, double intercept, double min, double max) {
+    public MyServo(Servo servo, String servoType, double loadMultiplier, double min, double max) {
         this.servo = servo;
-        this.positionPerRadian = ticksPerRadians;
-        this.intercept = intercept;
         this.min = min;
         this.max = max;
 
@@ -19,12 +17,12 @@ public class MyServo {
         }
         speed *= loadMultiplier;
     }
+
     double currentAngle = 0;
     double currentPosition = 0;
     double offset = 0; // ex: makes deposit bucket level
     long lastUpdateTime = System.nanoTime();
     public double positionPerRadian = 0.19098593171; //300 degrees is operating range so 1/(300*pi/180)
-    public double intercept = 0; // reassigns what 0 is for the servo
     double max = 1, min = 0;
 
     double lastPos = 0.0;
@@ -36,17 +34,17 @@ public class MyServo {
     public void setPosition(double targetPosition, double power) {
         targetPosition += offset;
         targetPosition = Math.max(Math.min(targetPosition,max),min);
-        double targetOrientation = (targetPosition - intercept) / positionPerRadian;
+        double targetOrientation = targetPosition / positionPerRadian; // converts position to radian angle
         double error = targetOrientation - currentAngle;
         long currentTime = System.nanoTime();
-        double time = (double)(currentTime - lastUpdateTime)/1.0E9;
+        double time = (double)(currentTime - lastUpdateTime)/1.0E9; // converts from nano to secs
         lastUpdateTime = currentTime;
-        double update = Math.signum(error) * speed * power * time; // update is the distance (in radians) the servo will move in a loop at the specified power
+        double update = Math.signum(error) * speed * power * time; // update is the distance (in radians) the servo has moved in a loop at the specified power
         if (Math.abs(update) >= Math.abs(error)) { // if setting servo position to update will cause the servo to go past it's target, set update = error
             update = error;
         }
         currentAngle += update;
-        currentPosition = currentAngle * positionPerRadian + intercept; // This converts the currentAngle (in radians) to a position value (between 0-1) and then adds the intercept
+        currentPosition = currentAngle * positionPerRadian; // This converts the currentAngle (in radians) to a position value (between 0-1) and then adds the intercept
         if (power == 1.0) {
             servo.setPosition(targetPosition);
         }
@@ -54,14 +52,13 @@ public class MyServo {
             servo.setPosition(currentPosition);
         }
     }
-    public void setAngle(double orientation, double power){
-        setPosition(orientation * positionPerRadian + intercept,power);
+
+    public void setAngle(double angle, double power){
+        setPosition(Math.toRadians(angle) * positionPerRadian,power);
     }
-    public void setPositionOrientation(double orientation){
-        setPosition(orientation * positionPerRadian + intercept,1.0);
-    }
-    public void setAngle(double orientation){
-        setPosition(orientation * positionPerRadian + intercept,1.0);
+
+    public void setAngle(double angle) {
+        setPosition(Math.toRadians(angle) * positionPerRadian, 1.0);
     }
     public double getAngle() {
         return currentAngle + offset/positionPerRadian;
@@ -71,7 +68,7 @@ public class MyServo {
         if(position != currentPos && !isMoving) {
             timeSinceNewPos = System.currentTimeMillis();
 
-            double setPosition = ((max-min) * position + min); // desmos
+            double setPosition = ((max-min) * position + min);
 
             servo.setPosition(setPosition);
             isMoving = true;
