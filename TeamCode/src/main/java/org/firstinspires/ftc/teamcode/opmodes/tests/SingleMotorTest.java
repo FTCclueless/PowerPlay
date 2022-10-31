185313f11889bd273d8868b6d90a684623df792b
hkim <hudson.n.kim@gmail.com>
2022-10-31 01:14:02 -0700

    added in SingleMotorTest + StrafeDrivetrainTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SingleMotorTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.modules.drive.Drivetrain;
+
+@Autonomous
+public class StrafeDrivetrainTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot robot = new Robot(hardwareMap);
+        Drivetrain drive = robot.drivetrain;
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            drive.setMotorPowers(0.15,-0.15,0.15,-0.15);
+            robot.update();
+        }
+    }
+}
