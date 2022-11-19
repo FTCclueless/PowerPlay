21198651ea4bd7c6a45fdf862589b4f9549b946a
hkim <hudson.n.kim@gmail.com>
2022-11-18 17:44:21 -0800

    WIP: Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
@@ -72,6 +72,12 @@ public class Auto extends LinearOpMode {
                 for (AprilTagDetection tag : currentDetections) {
                     // Add 1 because its 0-2 values not 1-3
                     parkingNum = tag.id + 1;
+                    if (parkingNum == 2) {
+                        parkingNum = 3;
+                    }
+                    else if (parkingNum == 3) {
+                        parkingNum = 2;
+                    }
                     detected = true;
                 }
 
@@ -94,12 +100,11 @@ public class Auto extends LinearOpMode {
         robot.currentState = Robot.STATE.IDLE;
 
         // 48 - 8 (width / 2) = 40
-        //                                                                                FIXME hasn't been tested
         Pose2d origin = new Pose2d((48 + tOffsetx) * xsign, (72 + tOffsety) * ysign, (-(Math.PI / 2) * ysign));
         drive.setPoseEstimate(origin);
 
         TrajectorySequence to = drive.trajectorySequenceBuilder(origin)
-                .strafeTo(new Vector2d((48 + tOffsetx) * xsign, (48 + tOffsety) * ysign))
+                .strafeTo(new Vector2d((48 + tOffsetx) * xsign, (0 + tOffsety) * ysign))
                 .strafeTo(new Vector2d((48 + tOffsetx) * xsign, (23 + tOffsety) * ysign))
                 .turn(-origin.getHeading())
                 .strafeTo(new Vector2d((36 + tOffsetx) * xsign, (23 + tOffsety) * ysign)) // Half tile back
@@ -128,20 +133,18 @@ public class Auto extends LinearOpMode {
 
         // (72, 24) if even amount of cycles and 36 if not
         Trajectory park = null;
-        Pose2d parkingOrigin = new Pose2d((36 + tOffsetx + ((targetCycles & 1) * 36)) * xsign, (23 + tOffsety) * ysign);
-        // Pose2d parkingOrigin = new Pose2d(36 + tOffsetx + ((targetCycles & 1) * 36), 23);
-        // Miscase for when it is already in its parking space
-        //if (parkingPos.getX() == parkingOrigin.getX() && parkingPos.getY() == parkingOrigin.getY()) {
+        Pose2d parkingOrigin = new Pose2d((36 + tOffsetx + ((targetCycles & 1) * 36)) * xsign, (23 + tOffsety) * ysign); // where the robot ends at the end of its cycles
+
+        if (parkingOrigin.getX() != parkingOrigin.getX() || parkingOrigin.getY() != parkingOrigin.getY() || parkingOrigin.getHeading() != parkingOrigin.getHeading()) {
             park = drive.trajectoryBuilder(parkingOrigin)
-                .strafeTo(parkingPos)
-                .build();
-        //}
+                    .strafeTo(parkingPos)
+                    .build();
+        }
 
         waitForStart();
 
         if (!isStopRequested()) {
             robot.followTrajectorySequence(to);
-            // FIXME jank
             for (int i = 0; i < targetCycles; i++) {
                 robot.followTrajectory(cycle1);
                 if (++i < targetCycles) {
