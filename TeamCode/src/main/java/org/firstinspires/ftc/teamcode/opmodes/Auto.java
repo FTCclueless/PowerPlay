6c929879208257514a79776a06e161efa3d47569
JustFast <justfast@github.com>
2022-11-27 19:54:21 -0800

    Update auto.java.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
@@ -1,27 +1,61 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import android.util.Log;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.modules.drive.Drivetrain;
 import org.firstinspires.ftc.teamcode.modules.drive.roadrunner.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.vision.AprilTagDetectionPipeline;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
 
 @Autonomous(group = "Test")
 public class Auto extends LinearOpMode {
     public static final int cycles = 5;
-    public static final boolean lr = true; // Left : true | Right : false
-    public static final boolean tb = true; // Top : true | Bottom : false
-    public static final double cycleBack = 12;
-    public static final double cycleY = 50.7; // Turning can give an offset (+ cone location)
+    public static int parkingNum = 0;
+    public static final boolean lr = false; // Left : true | Right : false
+    public static final boolean tb = false; // Top : true | Bottom : false
+    public static final double cycleBack = 6;
+    public static final double cycleY = 48; // Turning can give an offset (+ cone location)
+    public static OpenCvCamera camera;
+    public static final AprilTagDetectionPipeline atdp = new AprilTagDetectionPipeline(
+        0.166, // Size of april tag in meters
+        // These 4 values are calibration for the C920 webcam (800x448)
+        578.272,
+        578.272,
+        402.145,
+        221.506
+    );
 
     @Override
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(hardwareMap);
         Drivetrain drive = robot.drivetrain;
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        camera.setPipeline(atdp);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(640,480, OpenCvCameraRotation.UPRIGHT); // need to change for phone back camera
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                Log.e("error with vision", "");
+            }
+        });
 
         // Signs
         int xS = tb ? 1 : -1;
@@ -44,7 +78,7 @@ public class Auto extends LinearOpMode {
 
         Pose2d toEnd = to.end(); // Incredible naming!
         TrajectorySequence cycle = drive.trajectorySequenceBuilder(toEnd)
-                .strafeTo(new Vector2d(toEnd.getX() + (24 + cycleBack) * xS, toEnd.getY())) // +3 because it ends at -3
+                .strafeTo(new Vector2d(toEnd.getX() + (12 + cycleBack) * xS, toEnd.getY())) // +3 because it ends at -3
                 .strafeTo(new Vector2d(toEnd.getX(), toEnd.getY()))
                 .build();
 
@@ -53,10 +87,41 @@ public class Auto extends LinearOpMode {
          * The end position
          */
         Trajectory[] park = new Trajectory[]{
-            drive.trajectoryBuilder(toEnd).strafeTo(new Vector2d(origin.getX() + (24 * yS), origin.getY() - (cycleY * yS))).build(),
-            drive.trajectoryBuilder(toEnd).strafeTo(new Vector2d(origin.getX(), origin.getY() - (cycleY * yS))).build(),
-            drive.trajectoryBuilder(toEnd).strafeTo(new Vector2d(origin.getX() - (24 * yS), origin.getY() - (cycleY * yS))).build()
+            drive.trajectoryBuilder(toEnd).strafeTo(new Vector2d(origin.getX() + (23.5 + (tb ? 0 : 1.5)), origin.getY() - (cycleY * yS))).build(),
+            drive.trajectoryBuilder(toEnd).strafeTo(new Vector2d(origin.getX() - (2 * yS), origin.getY() - (cycleY * yS))).build(),
+            drive.trajectoryBuilder(toEnd).strafeTo(new Vector2d(origin.getX() - (25), origin.getY() - (cycleY * yS))).build()
         };
+
+        while (opModeInInit()) {
+            telemetry.setMsTransmissionInterval(50);
+
+            boolean detected = false;
+            ArrayList<AprilTagDetection> currentDetections = atdp.getLatestDetections();
+
+            if (currentDetections.size() != 0) {
+                for (AprilTagDetection tag : currentDetections) {
+                    switch (tag.id) {
+                        case 2:
+                            parkingNum = 1;
+                            break;
+                        case 1:
+                            parkingNum = 2;
+                            break;
+                        default:
+                            parkingNum = tag.id;
+                    }
+                    detected = true;
+                }
+            }
+
+            if (detected) {
+                telemetry.addLine(String.format("Tag of interest is in sight! ID: %d", parkingNum + 1));
+            } else {
+                telemetry.addLine("Could not find april tag! :(");
+            }
+
+            telemetry.update();
+        }
 
         robot.outtake.resetEncoders();
         waitForStart();
@@ -67,7 +132,7 @@ public class Auto extends LinearOpMode {
             for (int i = 0; i < cycles; i++) {
                 robot.followTrajectorySequence(cycle, this);
             }
-            robot.followTrajectory(park[0], this);
+            robot.followTrajectory(park[parkingNum], this);
         }
     }
 }
