858364a75df0cada3ed96cb3711fe2e058e9a2c5
hkim <hudson.n.kim@gmail.com>
2022-11-27 19:17:56 -0800

    added in 45 degree turret increments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/turret/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/turret/Turret.java
@@ -20,7 +20,7 @@ public class Turret {
 
     ArrayList<MotorPriority> motorPriorities;
 
-    public PID turretPID = new PID(3.0, 0.75,0.0);
+    public PID turretPID = new PID(6.0, 0.0,0.0);
 
     public double currentTurretAngle = 0.0;
     public double currentTurretVelocity = 0.0;
