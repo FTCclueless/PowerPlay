7ae8459999cba971b00f10da1130d78e610f25ee
JustFast <justfast@github.com>
2022-12-03 20:35:40 -0800

    Push Auto.java. Reflection not finished yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
@@ -27,11 +27,9 @@ import java.util.ArrayList;
 @Autonomous(group = "Test")
 public class Auto extends LinearOpMode {
     public static final int cycles = 5;
-    public static int parkingNum = 0;
+    public static int parkingNum = 0; // 0-2
     public static final boolean lr = true; // Left : true | Right : false
-    public static final boolean tb = true; // Top : true | Bottom : false
-    public static final double cycleBack = 6; // Once robot gets to cycle position how much it moves backwards
-    public static final double cycleY = 48; // Turning can give an offset (+ cone location)
+    public static final boolean tb = false; // Top : true | Bottom : false
     public static OpenCvCamera camera;
     public static final AprilTagDetectionPipeline atdp = new AprilTagDetectionPipeline(
         0.166, // Size of april tag in meters
@@ -47,64 +45,73 @@ public class Auto extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(hardwareMap);
         Drivetrain drive = robot.drivetrain;
-//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-//        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-//        camera.setPipeline(atdp);
-//        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-//            @Override
-//            public void onOpened() {
-//                camera.startStreaming(640,480, OpenCvCameraRotation.UPRIGHT); // need to change for phone back camera
-//            }
-//
-//            @Override
-//            public void onError(int errorCode) {
-//                Log.e("error with vision", "");
-//            }
-//        });
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        camera.setPipeline(atdp);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(640,480, OpenCvCameraRotation.UPRIGHT); // need to change for phone back camera
+            }
 
-        // Signs
+            @Override
+            public void onError(int errorCode) {
+                Log.e("error with vision", "");
+            }
+        });
+
         int xSign = tb ? 1 : -1;
         int ySign = lr ? 1 : -1;
 
         Pose2d origin = new Pose2d(
             36 * xSign,
             60 * ySign,
-            Math.PI / 2 * (!lr ? -1 : 1)
+            Math.PI / 2 * (lr ? 1 : -1)
         );
 
         drive.setPoseEstimate(origin);
 
-        double heading = Math.toRadians(tb ? 0 : 180);
-
         Pose2d intakePose = new Pose2d(
-                55,
-                9
+            55 * xSign,
+            10 * ySign,
+            tb ? 0 : Math.PI
         );
 
         Pose2d depositPose = new Pose2d(
-                38,
-                9
+            38 * xSign,
+            10 * ySign,
+            tb ? 0 : Math.PI
         );
 
         drive.setPoseEstimate(origin); // FIXME is this needed?
         TrajectorySequence to = drive.trajectorySequenceBuilder(origin)
-                // Move forward extra in order to bump away the signal cone
-                .strafeTo(new Vector2d(origin.getX(), origin.getY() - (54 * ySign)))
-                .lineToLinearHeading(new Pose2d(depositPose.getX(),depositPose.getY(), depositPose.getHeading()))
+                // Move forward extra in order to bump away the signal cone (and to give some leeway for the next trajectory)
+                .strafeTo(new Vector2d(origin.getX(), origin.getY() - 54))
+                .lineToLinearHeading(depositPose)
                 .build();
 
-        TrajectorySequence toDeposit = drive.trajectorySequenceBuilder(new Pose2d(intakePose.getX() - 3, intakePose.getY()))
-                .splineToConstantHeading(new Vector2d(depositPose.getX(),depositPose.getY()), heading)
+        // splineToConstantHeading will attempt to correct the robot's heading if it is bumped/moved
+        Trajectory toIntake = drive.trajectoryBuilder(to.end())
+                .splineToConstantHeading(new Vector2d(intakePose.getX(), intakePose.getY()), 0)
                 .build();
 
-        TrajectorySequence toIntake = drive.trajectorySequenceBuilder(new Pose2d(depositPose.getX() + 2, depositPose.getY()))
-                .splineToConstantHeading(new Vector2d(intakePose.getX(), intakePose.getY()), heading)
+        Trajectory toDeposit = drive.trajectoryBuilder(toIntake.end())
+                .splineToConstantHeading(new Vector2d(depositPose.getX(), depositPose.getY()), 0)
                 .build();
 
         Trajectory[] park = new Trajectory[]{
-            drive.trajectoryBuilder(toDeposit.end()).strafeTo(new Vector2d(origin.getX() + (23.5 + (tb ? 0 : 1.5)), origin.getY() - (cycleY * ySign))).build(),
-            drive.trajectoryBuilder(toDeposit.end()).strafeTo(new Vector2d(origin.getX() - (2 * ySign), origin.getY() - (cycleY * ySign))).build(),
-            drive.trajectoryBuilder(toDeposit.end()).strafeTo(new Vector2d(origin.getX() - (25), origin.getY() - (cycleY * ySign))).build()
+            drive.trajectoryBuilder(toDeposit.end()).strafeTo(new Vector2d(
+                    origin.getX() + 24,
+                    intakePose.getY() // deposit or intake would both work for this
+            )).build(),
+            drive.trajectoryBuilder(toDeposit.end()).strafeTo(new Vector2d(
+                    origin.getX(),
+                    intakePose.getY()
+            )).build(),
+            drive.trajectoryBuilder(toDeposit.end()).strafeTo(new Vector2d(
+                    origin.getX() - 24,
+                    intakePose.getY()
+            )).build()
         };
 
         robot.resetEncoders();
@@ -112,24 +119,25 @@ public class Auto extends LinearOpMode {
         while (opModeInInit()) {
             telemetry.setMsTransmissionInterval(50);
 
-//            boolean detected = false;
-//            ArrayList<AprilTagDetection> currentDetections = atdp.getLatestDetections();
-//
-//            if (currentDetections.size() != 0) {
-//                for (AprilTagDetection tag : currentDetections) {
-//                    switch (tag.id) {
-//                        case 2:
-//                            parkingNum = 1;
-//                            break;
-//                        case 1:
-//                            parkingNum = 2;
-//                            break;
-//                        default:
-//                            parkingNum = tag.id;
-//                    }
-//                    detected = true;
-//                }
-//            }
+            boolean detected = false;
+            ArrayList<AprilTagDetection> currentDetections = atdp.getLatestDetections();
+
+            if (currentDetections.size() != 0) {
+                for (AprilTagDetection tag : currentDetections) {
+                    // The april tags are sometimes wrong... I don't know why.
+                    switch (tag.id) {
+                        case 2:
+                            parkingNum = 1;
+                            break;
+                        case 1:
+                            parkingNum = 2;
+                            break;
+                        default:
+                            parkingNum = tag.id;
+                    }
+                    detected = true;
+                }
+            }
 
             robot.actuation.level();
             robot.outtake.extension.retractExtension();
@@ -138,13 +146,14 @@ public class Auto extends LinearOpMode {
             }
             robot.update();
 
-//            if (detected) {
-//                telemetry.addLine(String.format("Tag of interest is in sight! ID: %d", parkingNum + 1));
-//            } else {
-//                telemetry.addLine("Could not find april tag! :(");
-//            }
-//
-//            telemetry.update();
+            telemetry.addLine("Trajectories have been built! Ready to run!");
+            if (detected) {
+                telemetry.addLine(String.format("Tag of interest is in sight! ID: %d", parkingNum + 1));
+            } else {
+                telemetry.addLine("Could not find april tag! :(");
+            }
+
+            telemetry.update();
         }
 
         waitForStart();
@@ -157,11 +166,12 @@ public class Auto extends LinearOpMode {
             robot.currentState = Robot.STATE.RETRACT;
             robot.startIntakeGlobal(
                 toIntake.end(),
-                new Pose2d((72 - 4) * xSign,10.0 * ySign),
+                new Pose2d((72 - 4) * xSign,10.0),
                 coneStackAdditionalHeight * (4 - i)
             );
 
-            robot.followTrajectorySequence(toIntake, this);
+            // This goes first as we are already at the deposit
+            robot.followTrajectory(toIntake, this);
 
             while (robot.currentState == INTAKE_GLOBAL) {
                 robot.update();
@@ -169,18 +179,13 @@ public class Auto extends LinearOpMode {
 
             robot.drivetrain.setBreakFollowingThresholds(new Pose2d(2.5, 2.5, Math.toRadians(5)), toDeposit.end());
 
-            robot.startScoringGlobal(toDeposit.end(), new Pose2d(25.5 * xSign,0),29.25, ySign); // 36
-            robot.followTrajectorySequence(toDeposit, this);
-            robot.update();
-            while (robot.currentState == SCORING_GLOBAL || robot.currentState == DEPOSIT) {
+            robot.startScoringGlobal(toDeposit.end(), new Pose2d(25.5 * xSign,0),29.25, xSign); // 36
+            robot.followTrajectory(toDeposit, this);
+            do {
                 robot.update();
-            }
-
-            while (robot.outtake.slides.currentSlidesLength > 15) {
-                robot.update();
-            }
+            } while ((robot.currentState == SCORING_GLOBAL || robot.currentState == DEPOSIT) && robot.outtake.slides.currentSlidesLength > 15);
         }
-        robot.drivetrain.setBreakFollowingThresholds(new Pose2d(0.5, 0.5, Math.toRadians(5)), park[parkingNum].end());
+        robot.drivetrain.setBreakFollowingThresholds(new Pose2d(0.1, 0.1, Math.toRadians(2)), park[parkingNum].end());
 
         robot.followTrajectory(park[parkingNum], this);
     }
